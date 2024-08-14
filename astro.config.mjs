5a58c163926503c0dc82c40769e8d2b1fb38021f
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-14 19:41:07 +0200

    Add sanity client + some api tests in discography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
+  },
+  integrations: [
+    sanity({
+      projectId: 'pgwm690m',
+      dataset: 'production',
+      useCdn: false,
+    }),
+  ],
+  image: {
+    domains: ['astro.build', 'cdn.sanity.io'],
   }
 });
