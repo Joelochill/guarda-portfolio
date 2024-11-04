c2a7210d6be1660b625c2f6b6c92c5cf009ae50b
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-11-04 11:44:22 +0100

    Add Guarda's desired changes

--- src/middleware.js
+++
@@ -1,7 +0,0 @@
-import { defineMiddleware } from 'astro:middleware';
-
-export const onRequest = defineMiddleware( async (context, next) => {
-  // const urlPath = context.request.url;
-  console.log(context);
-  return next();
-});
