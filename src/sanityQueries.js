13e149638893e2a58d7e11d42038cf46a150a154
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-11-07 14:45:37 +0100

    Refactor sanity queries

---
+++ src/sanityQueries.js
@@ -0,0 +1,36 @@
+import imageUrlBuilder from '@sanity/image-url';
+import { sanityClient } from 'sanity:client';
+
+const builder = imageUrlBuilder(sanityClient);
+
+function urlFor(source) {
+  return builder.image(source);
+}
+
+// Projects' queries
+
+const rawProjects = await sanityClient.fetch(
+  `*[_type == "project"] | order(releaseDate desc) { 
+    title, cover, imageAlt, bandcampLink 
+  }`,
+);
+
+export const projects = rawProjects.map(({ cover, ...rest }) => ({
+  coverUrl: urlFor(cover).url(),
+  ...rest,
+}));
+
+// Live's queries & formatting
+
+const events = await sanityClient.fetch(
+  `*[_type == "event"] | order(date desc) { date, city, country, title, link }`,
+);
+
+const uniqueYears = new Set(
+  events.map((event) => new Date(event.date).getFullYear()),
+);
+
+export const liveContent = [...uniqueYears].map((year) => ({
+  year,
+  events: events.filter((event) => new Date(event.date).getFullYear() === year),
+}));
