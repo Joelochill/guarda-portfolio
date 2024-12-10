23eea2f9b49bab670dbdba5cc746172bf44113a9
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-10 14:06:39 +0100

    Update i18n logic to something i can code with

---
+++ src/i18n/utils.js
@@ -0,0 +1,12 @@
+import translations from './translations.json';
+
+export function useTranslations(lang) {
+  return function t(keys) {
+    return (
+      keys.split('.').reduce((acc, k) => acc?.[k], translations[lang]) ??
+      keys
+        .split('.')
+        .reduce((acc, k) => acc?.[k], translations[translations.defaultLang])
+    );
+  };
+}
