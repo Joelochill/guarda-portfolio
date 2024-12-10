826f763fe8f45a9609db1fa4ada82f18c2257217
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-10 11:39:36 +0100

    Reinstall node_modules + add new i18n features

---
+++ src/i18n/utils.ts
@@ -0,0 +1,17 @@
+/*
+  From this recipe: https://docs.astro.build/es/recipes/i18n/#traducir-strings-de-la-ui
+*/
+
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
+}
