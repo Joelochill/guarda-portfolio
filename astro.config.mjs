e577a11a6cdd498c4dda1bd6704345fd320f42d1
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-19 15:25:37 +0100

    Set up environment variables for config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,40 @@
-import sanity from '@sanity/astro';
-import { defineConfig } from 'astro/config';
-
-import icon from 'astro-icon';
-
 import sitemap from '@astrojs/sitemap';
+import sanity from '@sanity/astro';
+import icon from 'astro-icon';
+import { defineConfig, envField } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const { SITE_URL, SANITY_PROJECT_ID, SANITY_DATASET_NAME } = loadEnv(
+  process.env,
+  process.cwd(),
+  '',
+);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.joelricorada.com',
-  integrations: [sanity({
-    projectId: 'pgwm690m',
-    dataset: 'production',
-    useCdn: false,
-  }), icon(), sitemap()],
+  site: `${SITE_URL}`,
+  trailingSlash: 'never',
+  env: {
+    schema: {
+      PUBLIC_EMAILJS_KEY: envField.string({
+        context: 'client',
+        access: 'public',
+      }),
+      PUBLIC_GOOGLE_ANALYTICS_ID: envField.string({
+        context: 'client',
+        access: 'public',
+      }),
+    },
+  },
+  integrations: [
+    sanity({
+      projectId: `${SANITY_PROJECT_ID}`,
+      dataset: `${SANITY_DATASET_NAME}`,
+      useCdn: false,
+    }),
+    icon(),
+    sitemap(),
+  ],
   image: {
     domains: ['astro.build', 'cdn.sanity.io'],
   },
