ec4b4a6cf1e65fe7beda4d426676124b16722ce4
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-25 15:25:06 +0100

    Add eslint-plugin-compat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,17 @@ import pluginJs from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import compat from 'eslint-plugin-compat';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts, astro}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  compat.configs['flat/recommended'],
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
-    ignores: ['**/dist/*', '**/env.d.ts'],
+    ignores: ['**/.astro/*', '**/dist/*', '**/env.d.ts'],
   },
   {
     rules: {},
