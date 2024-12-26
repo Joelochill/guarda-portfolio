98b210dfb079fee8dbe75e202b01b5e0bb204c30
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-26 19:06:38 +0100

    Add stylelint-config-html + npm scripts

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,6 +2,12 @@
 export default {
   extends: ['stylelint-config-standard'],
   plugins: ['stylelint-no-unsupported-browser-features'],
+  overrides: [
+    {
+      files: ['*.astro', '**/*.astro'],
+      extends: ['stylelint-config-html/astro'],
+    },
+  ],
   rules: {
     'plugin/no-unsupported-browser-features': [true, { severity: 'warning' }],
   },
