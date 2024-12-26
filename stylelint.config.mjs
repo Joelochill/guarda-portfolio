85a229ba4060d66083d99dcc14a4357b7274c227
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-12-26 15:42:56 +0100

    Add stylelint & first configuration

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-no-unsupported-browser-features'],
+  rules: {
+    'plugin/no-unsupported-browser-features': [true, { severity: 'warning' }],
+  },
+};
