62eb4ec03712aa7e86922312976660f85dd29e75
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-01-28 18:45:26 +0100

    Add @prettier/plugin-xml

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,7 +5,7 @@
  * @type {import("prettier").Config}
  */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', '@prettier/plugin-xml'],
   singleQuote: true,
   overrides: [
     {
@@ -25,6 +25,16 @@ export default {
       files: '*.astro',
       options: {
         parser: 'astro',
+      },
+    },
+    {
+      files: '*.xml',
+      options: {
+        parser: 'xml',
+        tabWidth: 4,
+        printWidth: 100,
+        xmlWhitespaceSensitivity: 'ignore',
+        xmlSelfClosingSpace: true,
       },
     },
   ],
